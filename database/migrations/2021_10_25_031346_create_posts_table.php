f9097957adcf425b798957a7f19d626794d06e43
EC2 Default User <ec2-user@ip-172-31-83-30.ec2.internal>
2021-10-25 03:23:14 +0000

    generate model & controll post

--- database/migrations/2021_10_25_031346_create_posts_table.php
+++ database/migrations/2021_10_25_031346_create_posts_table.php
@@ -15,8 +15,6 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
-            $table->text('content');
             $table->timestamps();
         });
     }
